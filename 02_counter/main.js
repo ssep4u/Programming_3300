37da14f7bb3546dadcc03dc94bbbaf1ad2eafe3d
mirim <>
2026-03-20 10:04:20 +0900

    counter HTML, JS

---
+++ 02_counter/main.js
@@ -0,0 +1,19 @@
+let number = 0;
+
+// h1에 있는 숫자 요소를 가져오자
+// const resultH1 = document.getElementsByTagName("h1")[0];
+const resultH1 = document.getElementById("result");
+// button에 있는 + 요소를 가져오자
+// const plusButton = document.getElementsByTagName("button")[0];
+const plusButton = document.getElementsByClassName("plus")[0];
+
+// plusButton.addEventListener("click", () => {
+//     number++;
+//     resultH1.innerHTML = number;
+// });
+plusButton.onclick = () => {
+    number++;
+    resultH1.innerHTML = number;
+};
+
+
