9fe7e356a1c9db229085704fb7f63b88998a515d
mirim <>
2026-04-24 09:39:21 +0900

    TodoListApp deleteTodo()

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -33,12 +33,18 @@ function TodoListApp() {
             )
         )
     }
+    const deleteTodo = (id) => {
+        //todos에서 하나씩 꺼낸 todo. id가 다르면, 복사하자
+        setTodos((todos) => 
+            todos.filter((todo) => todo.id !== id)
+        )
+    }
 
     return (
         <div className="todo">
             <TodoHeader />
             <TodoAdder addTodo={addTodo} />
-            <TodoList todos={todos} toggleTodo={toggleTodo} />
+            <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
         </div>
     )
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,7 @@
 import TodoItemEmpty from './TodoItemEmpty.jsx'
 import TodoItem from './TodoItem.jsx'
 
-export default function TodoList({ todos, toggleTodo }) {
+export default function TodoList({ todos, ...rest }) {
     return (
         <ul className="todo__list">
             {/* todos에 값이 없으면, TodoItemEmpty */}
@@ -9,7 +9,7 @@ export default function TodoList({ todos, toggleTodo }) {
             {/* todos에 값이 있으면, TodoItem에 todo를 넣자 */}
             {todos.length > 0 &&
                 todos.map((todo) =>
-                    <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} />
+                    <TodoItem key={todo.id} todo={todo} {...rest} />
                 )
             }
         </ul>
