6669b0401b06065ceafb850b003d734597aa377f
jaehokang1007 <jaehokang1007@icloud.com>
2026-05-29 09:18:52 +0900

    fix: 노출된 API KEY 제거 및 env 설정 추가

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -2,10 +2,11 @@ import { useEffect, useState } from "react";
 
 export default function Weather() {
   const [weather, setWeather] = useState(null);
+  const API_KEY = import.meta.env.VITE_API_KEY;
 
   useEffect(() => {
     fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=833be9eeecbdea0cbb10df46b92359ed&units=metric&lang=kr",
+      `https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=${API_KEY}&units=metric&lang=kr`,
     )
       .then((res) => res.json())
       .then((data) => setWeather(data));
@@ -18,7 +19,7 @@ export default function Weather() {
 
   return (
     <>
-      <h1>{weather.cityName}</h1> {/* 도시 이름 */}
+      <h1>{weather.name}</h1> {/* 도시 이름 */}
       <img src={iconUrl} alt="날씨" /> {/* 날씨 아이콘 */}
       <p>{weather.weather[0].description == "박무" ? "옅은 안개" : weather.weather[0].description}</p> {/* 날씨 설명 */}
       <h2>{weather.main.temp}°C</h2> {/* 날씨 온도 */}
