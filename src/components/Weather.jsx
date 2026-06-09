710d83ef8919fd67c10aa2831f35392f282a980a
jaehokang1007 <jaehokang1007@icloud.com>
2026-06-10 08:38:20 +0900

    - 3301 강재호 1-9	날씨	★★★★	useEffect, fetch, 외부 API.

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -21,7 +21,7 @@ export default function Weather() {
     <>
       <h1>{weather.name}</h1> {/* 도시 이름 */}
       <img src={iconUrl} alt="날씨" /> {/* 날씨 아이콘 */}
-      <p>{weather.weather[0].description == "박무" ? "옅은 안개" : weather.weather[0].description}</p> {/* 날씨 설명 */}
+      <p>{weather.weather[0].description}</p> {/* 날씨 설명 */}
       <h2>{weather.main.temp}°C</h2> {/* 날씨 온도 */}
     </>
   );
